e17b6f2311a756a4d3ad5283eb24516fdbe9a62e
Salem Louy <98494370+simo0loufi@users.noreply.github.com>
2024-06-23 10:05:15 +0200

    add google form link
    add google form link

--- app/_components/Button.jsx
+++ app/_components/Button.jsx
@@ -4,7 +4,7 @@ export default function Button({title,styling}) {
   return (
     <div >
     <a
-      href="#"
+      href="https://forms.gle/CpuZGc25DATucxHx6" target='_blanck'
       className={styling === 'primary' ? "inline-flex gap-2 rounded border border-white bg-secondry px-12 py-3 text-sm font-medium text-primary transition duration-500 hover:bg-primary hover:text-secondry"
         :"inline-flex gap-2 rounded border border-white bg-primary  px-12 py-3 text-sm font-medium text-secondry transition duration-500 hover:bg-secondry hover:text-primary "}
     ><span aria-hidden="true" className="block transition-all group-hover:ms-0.5 rtl:rotate-180">&larr; </span>
