3b94c5e2d6a31b612229e799bca43fe31857c078
Salem Louy <98494370+simo0loufi@users.noreply.github.com>
2024-06-23 10:32:06 +0200

    Update ContactUs.jsx

--- app/_components/ContactUs.jsx
+++ app/_components/ContactUs.jsx
@@ -9,13 +9,13 @@ export default function
         <Section title="تواصل معنا" desc="تواصل معنا للاستفسار و طلب خدماتنا عبر وسائل التواصل التالية"/>
         <div className='mt-8 grid md:w-[700px] mx-8 justify-items-center grid-cols-1 gap-4 sm:grid-cols-3 md:gap-2'>
           {contactUsData.map((contact,index)=>(
-              <div className="bg-secondry  w-[180px] max-w-sm border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <div className="bg-secondry  w-[180px] max-w-sm border border-gray-200 rounded-lg shadow">
               
               <div className="flex flex-col items-center py-10">
                     <svg className="h-20 w-20" fill={contact.color} viewBox="0 0 24 24" aria-hidden="true">
                       {contact.pathsvg}
                     </svg>
-                  <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">{contact.socielmedia}</h5>
+                  <h5 className="mb-1 text-xl font-medium text-gray-900 ">{contact.socielmedia}</h5>
                   <span className="text-sm text-gray-500 dark:text-gray-400">{contact.userName}</span>
                   {contact.socielmedia === 'whatsapp'?
                   <div className="flex mt-4 md:mt-6">
